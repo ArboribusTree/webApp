32874c9607d1c87c55bcd43866ab99db65118a6e
Ichimoku27 <145424093+Ichimoku27@users.noreply.github.com>
2023-12-05 01:38:01 +0800

    Add files via upload

--- routes/post.js
+++ routes/post.js
@@ -19,8 +19,42 @@ router.post('/createComments', postController.createComments)
 
 router.get('/', postController.getPosts) 
 
-router.post('/:id/upvotePost', postController.upvotePost)
 
-router.post('/:id/downvotePost', postController.downvotePost)
+router.get('/edit/:id', postController.editPost);
+
+
+router.post('/:id/edit', postController.updatePost);
+
+
+router.post('/:id/delete', async (req, res) => {
+    const postId = req.params.id
+
+    try {
+        console.log('Deleting post with ID:', postId);
+
+        const post = await Post.findById(postId)
+
+       
+        if (post.author.equals(req.session.user._id)) {
+            
+            await Post.deleteOne({ _id: postId })
+            console.log('Post deleted successfully.')
+            res.redirect('/posts')
+        } else {
+            console.log('User is not authorized to delete this post.')
+            
+            res.status(403).send('Forbidden')
+        }
+    } catch (error) {
+        console.error('Error deleting post:', error)
+        res.status(500).send('Internal Server Error')
+    }
+})
+
+router.get('/:postId/comments/edit/:id', postController.getEditCommentPage)
+
+router.post('/:postId/comments/:id/edit', postController.updateComment)
+
+router.post('/:postId/comments/:id/delete', postController.deleteComment)
 
 module.exports = router 
